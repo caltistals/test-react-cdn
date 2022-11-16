845985f01710e3dbde71187879d8fe96ac81b03f
caltistals <y.db.qp.y@gmail.com>
2022-11-16 22:13:17 +0900

    Update APIを叩く処理を追加

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 import Child from "./components.js";
 const useState = React.useState;
-// const useEffect = React.useEffect;
+const useEffect = React.useEffect;
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const [users, setUsers] = useState(null);
   const increment = () => setCount((prev) => prev + 1);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+        setIsLoading(false);
+      });
+  }, []);
 
   return (
-    <div className="wow">
-      <p>react CDN</p>
-      <p>count: {count}</p>
-      <button onClick={increment}>count up</button>
-      <Child />
+    <>
+      <div className="wow">
+        <p>react CDN</p>
+        <p>count: {count}</p>
+        <button onClick={increment}>count up</button>
+        <Child />
+      </div>
+      <div>
+        {isLoading && <p>Loading...</p>}
+        {users && users.map((user) => <User user={user} />)}
+      </div>
+    </>
+  );
+};
+
+const User = (props) => {
+  const { user } = props;
+  return (
+    <div>
+      id: {user.id}, name: {user.name}, email: {user.email}
     </div>
   );
 };
