ed294db1305280d358e909fcd2a148d8b25d2b59
caltistals <y.db.qp.y@gmail.com>
2022-11-22 16:39:33 +0900

    [update] 補足説明を追加

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ const Search = () => {
   return (
     <>
       <p>アルバムを検索</p>
+      <p>(1-100の数字を入力するとアルバム情報を取得できます)</p>
       <form action="/result.html" method="get">
         <input value={value} name="value" onChange={handleValueChange} />
         <input type="submit" value="検索！" />
