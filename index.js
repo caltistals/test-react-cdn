aeed5ae50b4b305975ebf080c2cdae6ff52752b6
caltistals <y.db.qp.y@gmail.com>
2022-11-22 16:33:52 +0900

    [update] 検索画面コンポーネントを作成

--- index.js
+++ index.js
@@ -1,48 +1,63 @@
-import Child from "./components.js";
 const useState = React.useState;
 const useEffect = React.useEffect;
 
-const App = () => {
-  const [count, setCount] = useState(0);
-  const [users, setUsers] = useState(null);
-  const increment = () => setCount((prev) => prev + 1);
-  const [isLoading, setIsLoading] = useState(false);
+// const App = () => {
+//   const [count, setCount] = useState(0);
+//   const [users, setUsers] = useState(null);
+//   const increment = () => setCount((prev) => prev + 1);
+//   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    setIsLoading(true);
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((data) => {
-        setUsers(data);
-        setIsLoading(false);
-      });
-  }, []);
+//   useEffect(() => {
+//     setIsLoading(true);
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setUsers(data);
+//         setIsLoading(false);
+//       });
+//   }, []);
 
+//   return (
+//     <>
+//       <div className="wow">
+//         <p>react CDN</p>
+//         <p>count: {count}</p>
+//         <button onClick={increment}>count up</button>
+//         <Child />
+//       </div>
+//       <div>
+//         {isLoading && <p>Loading...</p>}
+//         {users && users.map((user) => <User user={user} />)}
+//       </div>
+//     </>
+//   );
+// };
+
+const Search = () => {
+  const [value, setValue] = useState("");
+  const handleValueChange = (event) => {
+    setValue(event.target.value);
+  };
   return (
     <>
-      <div className="wow">
-        <p>react CDN</p>
-        <p>count: {count}</p>
-        <button onClick={increment}>count up</button>
-        <Child />
-      </div>
-      <div>
-        {isLoading && <p>Loading...</p>}
-        {users && users.map((user) => <User user={user} />)}
-      </div>
+      <p>アルバムを検索</p>
+      <form action="/result.html" method="get">
+        <input value={value} name="value" onChange={handleValueChange} />
+        <input type="submit" value="検索！" />
+      </form>
     </>
   );
 };
 
-const User = (props) => {
-  const { user } = props;
-  return (
-    <div>
-      id: {user.id}, name: {user.name}, email: {user.email}
-    </div>
-  );
-};
+// const User = (props) => {
+//   const { user } = props;
+//   return (
+//     <div>
+//       id: {user.id}, name: {user.name}, email: {user.email}
+//     </div>
+//   );
+// };
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(<App />);
+root.render(<Search />);
