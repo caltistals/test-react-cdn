8605e9a2a2f530d5513215fdd07b82f0365bfec8
caltistals <y.db.qp.y@gmail.com>
2022-12-02 22:31:43 +0900

    [clean] 不要なコメントアウトを削除

--- index.js
+++ index.js
@@ -1,38 +1,6 @@
 const useState = React.useState;
 const useEffect = React.useEffect;
 
-// const App = () => {
-//   const [count, setCount] = useState(0);
-//   const [users, setUsers] = useState(null);
-//   const increment = () => setCount((prev) => prev + 1);
-//   const [isLoading, setIsLoading] = useState(false);
-
-//   useEffect(() => {
-//     setIsLoading(true);
-//     fetch("https://jsonplaceholder.typicode.com/users")
-//       .then((response) => response.json())
-//       .then((data) => {
-//         setUsers(data);
-//         setIsLoading(false);
-//       });
-//   }, []);
-
-//   return (
-//     <>
-//       <div className="wow">
-//         <p>react CDN</p>
-//         <p>count: {count}</p>
-//         <button onClick={increment}>count up</button>
-//         <Child />
-//       </div>
-//       <div>
-//         {isLoading && <p>Loading...</p>}
-//         {users && users.map((user) => <User user={user} />)}
-//       </div>
-//     </>
-//   );
-// };
-
 const Search = () => {
   const [value, setValue] = useState("");
   const handleValueChange = (event) => {
@@ -49,15 +17,6 @@ const Search = () => {
     </>
   );
 };
-
-// const User = (props) => {
-//   const { user } = props;
-//   return (
-//     <div>
-//       id: {user.id}, name: {user.name}, email: {user.email}
-//     </div>
-//   );
-// };
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
