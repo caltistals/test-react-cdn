bceb7c34cd58af064b9bfaec22e72f99b43d098f
caltistals <y.db.qp.y@gmail.com>
2022-12-02 22:38:31 +0900

    [update] SearchコンポーネントにHeaderを追加

--- search.js
+++ search.js
@@ -8,13 +8,34 @@ const Search = () => {
   };
   return (
     <>
-      <p>アルバムを検索</p>
+      <Header />
+      <h1>アルバムを検索</h1>
       <p>(1-100の数字を入力するとアルバム情報を取得できます)</p>
       <form action="/result.html" method="get">
         <input value={value} name="value" onChange={handleValueChange} />
         <input type="submit" value="検索！" />
       </form>
     </>
+  );
+};
+
+const Header = () => {
+  return (
+    <header>
+      <a href="/index.html" className="logo">
+        Billboard Kindai
+      </a>
+      <nav>
+        <ul className="nav-list">
+          <li className="nav-list-item">
+            <a href="/index.html">ホーム</a>
+          </li>
+          <li clasName="nav-list-item">
+            <a href="/search.html">検索</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
